93dc365796435df4dbc46f1bb225660a67d3822a
Ty4jbt <tyaboe@gmail.com>
2021-06-24 08:46:23 -0700

    changed cors

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ const express = require('express'),
 
 let allowedOrigins = ['http://localhost:8080', 'https://boemyflix.herokuapp.com/'];
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1) {
-            let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-            return callback(new Error(message ), false);
-        }
-        return callback(null, true);
-    }
-}));
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if(!origin) return callback(null, true);
+//         if(allowedOrigins.indexOf(origin) === -1) {
+//             let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+//             return callback(new Error(message ), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
+app.use(cors())
 app.use(express.static('public'));
 app.use(express.json());
 app.use(morgan('common'));
